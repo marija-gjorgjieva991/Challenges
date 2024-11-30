267c9fa562a0712492fab6a392f75b724eedc091
Marija Gjorgjieva <marija_gjorgjieva@yahoo.com>
2024-11-30 12:40:14 +0100

    DetailsBlock component done

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Banner from "./components/Banner";
+import DetailsBlock from "./components/DetailsBlock";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Banner />
+      <DetailsBlock />
     </div>
   );
 };
--- starter/src/components/DetailsBlock.tsx
+++ starter/src/components/DetailsBlock.tsx
@@ -1,5 +1,25 @@
 const DetailsBlock = () => {
-  return <></>;
+  return (
+    <>
+      <div className="block-details">
+        <div className="block-content">
+          <span>About</span>
+          <h2>Stories of Adventure</h2>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
+            ratione nostrum ea obcaecati suscipit quia magnam!Sed, eveniet vel
+            reprehenderit tenetur minima ad aliquid velit quibusdam earum
+            aspernatur doloremque ipsum!
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facere
+            ratione nostrum ea obcaecati suscipit quia magnam!
+          </p>
+        </div>
+        <img src="../public/1018-3914x2935.jpg" alt=""></img>
+      </div>
+    </>
+  );
 };
 
 export default DetailsBlock;
