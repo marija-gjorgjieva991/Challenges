bfd7ce09545d7d1fc2c300d04b2a069df84132fb
Marija Gjorgjieva <marija_gjorgjieva@yahoo.com>
2024-11-30 15:35:58 +0100

    PlacesContainer is done

--- starter/src/components/Place.tsx
+++ starter/src/components/Place.tsx
@@ -1,5 +1,20 @@
-const Place = () => {
-  return <></>;
+export interface PlaceType {
+  img: string;
+  place: string;
+  desc: string;
+}
+const Place = ({ img, place, desc }: PlaceType) => {
+  return (
+    <>
+      <div className="place-div">
+        <img src={img} alt={place} />
+        <div className="place-content">
+          <h2>{place}</h2>
+          <p>{desc}</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Place;
--- starter/src/components/PlacesContainer.tsx
+++ starter/src/components/PlacesContainer.tsx
@@ -1,8 +1,34 @@
- import Place from "./Place";
- const PlacesContainer = () => {
-  return <>
-  <Place />
-  </>;
+import { useEffect, useState } from "react";
+import Place, { PlaceType } from "./Place";
+
+const PlacesContainer = () => {
+  const [places, setPlaces] = useState<PlaceType[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("http://localhost:5173/src/data/db.json");
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setPlaces(data.places);
+      } catch (error) {
+        ("Error fetching places data");
+      }
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <>
+      <div className="places-container">
+        {places.map((place, index) => (
+          <Place key={`place-${index}`} {...place} />
+        ))}{" "}
+      </div>
+    </>
+  );
 };
 
 export default PlacesContainer;
