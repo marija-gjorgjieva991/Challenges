bfb293800f496777fd39c71c64b8ad0d44dc4349
Marija Gjorgjieva <marija_gjorgjieva@yahoo.com>
2024-11-30 15:44:26 +0100

    Footer done

--- starter/src/components/Footer.tsx
+++ starter/src/components/Footer.tsx
@@ -1,5 +1,11 @@
 const Footer = () => {
-  return <></>;
+  return (
+    <div className="footer">
+      <a href="/">Destinations</a>
+      <a href="/">Contact</a>
+      <a href="/">Stories</a>
+    </div>
+  );
 };
 
 export default Footer;
