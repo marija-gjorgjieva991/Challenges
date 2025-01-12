4636a05c4ab194b613b38fd0ca2b003736476779
Marija Gjorgjieva <marija_gjorgjieva@yahoo.com>
2025-01-12 17:39:44 +0100

    Challenge finished

--- starter/Artist Gallery/src/Components/Navbar.tsx
+++ starter/Artist Gallery/src/Components/Navbar.tsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 const Navbar: React.FC = () => {
   return (
-    <>
+    <header>
       <Link to="/" className="navLink">
         <div className="nav">
           <h1>Music Db</h1>
         </div>
       </Link>
       <hr />
-    </>
+    </header>
   );
 };
 
